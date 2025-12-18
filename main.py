6bbb8d8482e53d0176285198d91fd576f3315657
amonaghan715 <amonaghan@bowdoin.edu>
2025-12-18 18:07:54 -0500

    added line search action to main loop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from folder_loader import Loader
-from searcher import Searcher
+from searcher import WordSearcher, LineSearcher
 
 def main():
     """The main function to run the system."""
@@ -11,10 +11,13 @@ def main():
         load_and_train = False
 
     keepGoing = True
+    data_file = "data.jsonl"
     loader = Loader(load_and_train)
     loader.parse_folder()
     wv_model, ft_model = loader.get_models()
-    searcher = Searcher(wv_model, ft_model)
+
+    word_search = WordSearcher(wv_model, ft_model)
+    line_search = LineSearcher(data_file, word_search)
 
     while(keepGoing):
         print("1. Run the Shakespearean thesaurus")
@@ -28,16 +31,48 @@ def main():
                 repeat = False
                 input_word = input("Please enter a word to search for: ")
 
-                words = searcher.n_similar_words(input_word, 7)
+                words = word_search.n_similar_words(input_word, 7)
                 if words:
                     for word, score in words:
                         print(f"{word} (Similarity score: {round(score, 3)})")
                 else:
-                    print("Unknown word. Wouldst thou provide a synonym?\n")
+                    print("Unknown word.\nWouldst thou provide a synonym?\n")
                     repeat = True
-                print("\n")
-        #elif "2" in action:
+            print("\n")
+        elif "2" in action:
+            repeat = True
+            while(repeat):
+                repeat = False
+                search_word = input("Please enter a theme to search for: ")
 
+                filters = {}
+                query = input("Would you like to add a search filter? (y/n): ").strip()
+                if "y" in query.lower():
+                    tag = input("Enter the filter tag (options: play_id, speaker, act, scene): ").strip()
+                    spec = input("Enter your specification (romeo-and-juliet, JULIET, 3, etc.): ").strip()
+
+                    # Clean tag and spec entries to match expected forms.
+                    if tag == 'play_id':
+                        spec = spec.lower()
+                    elif tag == 'speaker':
+                        spec = spec.upper()
+                    elif tag == 'act' or tag == 'scene':
+                        spec = int(spec)
+
+                    filters[tag] = spec
+                
+                hits = line_search.search(query=search_word, expand=True, filters=filters)
+                if hits:
+                    for hit in hits:
+                        print(hit)
+                else:
+                    print("Unknown word or no lines that match that theme.")
+                    print("Wouldst thou provide a synonym?\n")
+                    repeat = True
+        #elif "3" in action:
+            
+        else:
+            print("Unknown action. Please enter the action you would like to take.\n")
 
 
 if __name__ == "__main__":
